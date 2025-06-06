8779a183f683bc26445e3130a3335d33447fab7d
Vinicius <viniciusbeira2@gmail.com>
2025-06-06 17:58:28 -0300

    Layout home e componentes da home concluídos

---
+++ src/app/(public)/_components/header.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { useState } from 'react'
+import Link from "next/link"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { Menu, LogIn } from "lucide-react"
+
+export function Header(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const session = null;
+
+    const navItems = [
+        { href: "#profissionais", label: "Profissionais" }
+    ]
+
+    const NavLinks = () => (
+        <>
+            {navItems.map((item) => (
+                <Button 
+                key={item.href} 
+                asChild 
+                className="bg-transparent hover:bg-transparent text-black shadow-none"
+                onClick={() => setIsOpen(false)}
+                >
+                    <Link href={item.href} className='text-base'>
+                        {item.label}
+                    </Link>
+                </Button>
+            ))}
+
+            {session ? (
+                <Link href="/dashboard" className='flex items-center justify-center gap-2'>
+                    Painel da Clinica
+                </Link>
+            ) : ( 
+                <Button onClick={() => setIsOpen(false)}>
+                    <LogIn />
+                    Portal da Clinica
+                </Button> )}
+
+        </>
+    )
+
+    return (
+        <header className="fixed top-0 right-0 left-0 z-[999] py-4 px-6 bg-white">
+            <div className="container mx-auto flex items-center justify-between">
+                <Link href="/" className="text-3xl font-bold text-zinc-900">
+                    Odonto<span className="text-emerald-500">PLUS</span>
+                </Link>
+
+                <nav className="hidden md:flex items-center space-x-4">
+                    <NavLinks />
+                </nav>
+
+                <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                    <SheetTrigger asChild className="md:hidden">
+                        <Button
+                        className="text-black hover:bg-transparent" 
+                        variant="ghost" 
+                        size="icon"
+                        >
+                            <Menu className="w-6 h-6"/>
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent side="right" className="w-[240px] sm:w-[300px] z-[9999] py-4 px-6">
+                        <SheetTitle>Menu</SheetTitle>
+                        <SheetHeader />
+                        <SheetDescription>
+                            Veja nossos links
+                        </SheetDescription>
+                        <nav className='flex flex-col space-y-4 mt-6'>
+                            <NavLinks />
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+
+            </div>
+        </header>
+    )
+}
